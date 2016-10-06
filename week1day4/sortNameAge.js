c0c18d0b91821dd8726adced2b5b468012cbf35a
DeeKeilholz <deekeilholz@gmail.com>
2016-10-06 18:11:37 +0000

    Adds sorting by name and age exercise

--- week1day4/sortNameAge.js
+++ week1day4/sortNameAge.js
@@ -16,10 +16,3 @@ students.sort(function(a,b) {
 })
 
 console.log(students);
-
-
-//Sort numerically and ascending:
-var myarray=[25, 8, 7, 41]
-myarray.sort(function(a,b){ //Array now becomes [7, 8, 25, 41]
-    return a - b
-})
