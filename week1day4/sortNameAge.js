98610321a8db2dd68e803194e1a857f3cfcaf98a
DeeKeilholz <deekeilholz@gmail.com>
2016-10-06 17:22:26 +0000

    Adds SortingByNameAndAge exercise

---
+++ week1day4/sortNameAge.js
@@ -0,0 +1,25 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function(a,b) {
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+  return b.age - a.age;
+})
+
+console.log(students);
+
+
+//Sort numerically and ascending:
+var myarray=[25, 8, 7, 41]
+myarray.sort(function(a,b){ //Array now becomes [7, 8, 25, 41]
+    return a - b
+})
